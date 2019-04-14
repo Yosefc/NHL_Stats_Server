e47628911db87af5425860726d00a8e40b9a3cc0
Yosefc <yosef.conn@gmail.com>
2019-04-14 16:41:08 +0300

    Added tests with jest.

---
+++ teamLogos.test.js
@@ -0,0 +1,17 @@
+const { getTeamLogo } = require("./teamLogos");
+
+describe("Team Logos", () => {
+  test("Should return a strimg", () => {
+    expect(getTeamLogo(9)).toBe(
+      `<img src="http://www.capsinfo.com/images/NHL_Team_Logos/NHL_Senators_Primary.png" height="42" width="42" style="object-fit: contain;">`
+    );
+  });
+
+  test("Image src should match NHL_Senators_Primary", () => {
+    expect(getTeamLogo(9)).toMatch(/NHL_Senators_Primary/);
+  });
+
+  test("Should return an img tag", () => {
+    expect(getTeamLogo(54)).toMatch(/(<img)|(>$)/g);
+  });
+});
